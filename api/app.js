bc393bb6ed8644aeebdf8a1524180497a1ef739d
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-15 19:20:09 +0300

    feat: update server with middleware to serve client files

--- api/app.js
+++ api/app.js
@@ -1,6 +1,10 @@
-import { Hono } from "./deps.js";
+import { Hono, serveStatic } from "./deps.js";
 
 const app = new Hono();
+
+app.use("/", serveStatic({ path: './static/index.html' }))
+app.use("/favicon.png", serveStatic({ path: './static/favicon.png' }))
+app.use("/_app/*", serveStatic({ root: './static/' }))
 
 app.get("/api/random", (c) => c.json({
   random: Math.random()
